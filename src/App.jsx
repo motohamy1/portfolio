419ec975d4c63995cd76fad64d74eb8385398295
motohamy1 <eltohamym660@gmail.com>
2025-07-02 17:41:57 +0300

    error handle

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
-import Services from "./components/services/services";
-import Resume from "./components/Resume/Resume";
-import Projects from "./components/Projects/Projects";
-import Contact from "./components/Contact/Contact";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Hero from "./components/Hero/Hero.jsx";
+import Services from "./components/services/services.jsx";
+import Resume from "./components/Resume/Resume.jsx";
+import Projects from "./components/Projects/Projects.jsx";
+import Contact from "./components/Contact/Contact.jsx";
 
 const App = () => {
   return (
